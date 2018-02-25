82a012c101f054867f8f408d8d92f52f91ad7285
Phil Smith <phil.smith@volarisgroup.com>
2018-02-24 22:23:41 -0500

    EOD 02/24/2018

--- dist/Attendance.js
+++ dist/Attendance.js
@@ -5,9 +5,9 @@ var store = new Vuex.Store({
       _selectedPeriod: "",
       _selectedMonth:0,
       _newreport:false,
-      _revenue:[],
-      _weeks:5,
-      _days:0
+      _savedreport:false,
+      _dataloded: false,
+      _savedData:null
     },
     getters:{
         getMonths: function(state){
@@ -21,14 +21,81 @@ var store = new Vuex.Store({
         },
         getNewReport: function(state){
             return state._newreport;
+        },
+        getSavedReport: function(state){
+            return state._savedreport;
+        },
+        getSavedReportData: function(state){
+            return state._savedData;
+        },
+        getDataLoaded: function(state){
+            return state._dataloded;
         }
     },
     mutations:{
         setNewReport: function(state, payload){
             state._newreport = payload;
         },
+        setSavedReport: function(state, payload){
+            state._savedreport = payload;
+        },
         setPeriod: function(state, payload){
             state._selectedPeriod = payload;
         },
+        setSavedData: function(state, payload){
+            state._savedData = payload;
+        },
+        setDataLoaded: function(state, payload){
+            state._dataloded = payload;
+        }
+    },
+    actions:{
+        getSavedData: function(context, payload){
+            var _data = {
+                ssattendance:payload.AverageSS,
+                ssoffering:payload.SSOffering,
+                ypeattendance:payload.AverageYPE,
+                ypeoffering:payload.YPEOffering,
+
+                regattendance:payload.AverageRegularService,
+                offering:payload.MonthOffering,
+                messages:payload.Preached,
+                tithes:payload.MonthTithes,
+
+                bibleclass:payload.BibleClass,
+                fasting:payload.Fasting,
+                homevisits:payload.HomeVisits,
+
+                streetservice:payload.StreetServices,
+                revivalservice:payload.RevivalServices,
+                converts:payload.Converts,
+                baptized:payload.Baptized,
+
+                added:payload.NewMembers,
+                excommunicated:payload.Excommunicated,
+                membership:payload.TotalMembers,
+                mens:payload.MenServices,
+
+                womens:payload.WomenServices,
+                youth:payload.YouthServices,
+                mensoffering:payload.MensOffering,
+                womensoffering:payload.WomensOffering,
+                youthoffering:payload.YouthOffering,
+
+                specialoffering:payload.SpecialOffering,
+                rallyoffering:payload.RallyOffering,
+                pastoroffering:payload.PastorExpenses,
+                mortgagamount:payload.Mortgage,
+                rentamount:payload.Rent,
+                utilitiesamount:payload.Utilities,
+
+                donationamout:payload.Donations,
+                miscamount:payload.MiscExpenses,
+                hqamount:payload.HQAmount,
+                comments:payload.Comments
+            }
+            context.commit("setSavedData", _data);
+            context.commit("setDataLoaded", true);
+        }
     }
 })
